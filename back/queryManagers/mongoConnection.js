66bca14169ad376131f838436ceb91757b60d92e
Aymaneelfiguigui <90783893+Aymaneelfiguigui@users.noreply.github.com>
2023-02-24 12:16:06 +0100

    fix routing and css

--- back/queryManagers/mongoConnection.js
+++ back/queryManagers/mongoConnection.js
@@ -3,29 +3,6 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://admin:admin@mongodb/admin?directConnection=true';
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// async function findInDataBase(response,currentUser,collectionName) {
-//     try {
-//         await client.connect();
-//         console.log('Connected to MongoDB');
-//         const db = client.db("connect4");
-//         //await db.addUser("admin", "admin", {roles: [{role: "readWrite", db: "connect4"}]});
-//         const collection = db.collection(collectionName);
-//         console.log(currentUser);
-//         const item = await collection.findOne(currentUser);
-//
-//         console.log(item);
-//         response.writeHead(200, {'Content-Type': 'application/json'});
-//         response.end(JSON.stringify(item));
-//
-//     } catch (err) {
-//         console.error('Failed to create database or user', err);
-//         response.status(401).send('Invalid username or password.');
-//         response.writeHead(400, {'Content-Type': 'application/json'});
-//         response.end(JSON.stringify({ status: 'failure' }));
-//     } finally {
-//         await client.close();
-//     }
-// }
 
 async function findInDataBase(response, currentUser, collectionName) {
     try {
