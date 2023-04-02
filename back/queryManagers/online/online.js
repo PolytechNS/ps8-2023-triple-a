732812ba9373a598f0aab18f596098a9d99e9700
Aymaneelfiguigui <90783893+Aymaneelfiguigui@users.noreply.github.com>
2023-04-02 21:54:19 +0200

    update elo formula

--- back/queryManagers/online/online.js
+++ back/queryManagers/online/online.js
@@ -88,61 +88,61 @@ async function manageRequest(request, response) {
     });
         }
 
-        if(filePath[3] === "updateScore"){
+        if (filePath[3] === "updateScore") {
             request.on('end', async function () {
                 const data = JSON.parse(body);
-
                 try {
                     console.log("before connecting to mongo")
                     await client.connect();
                     console.log('Connected to MongoDB in gamification process');
                     const db = client.db('connect4');
                     const collection = db.collection('log');
-
-                    const current = await collection.findOne({token : data.winner});
-                    const opponent = await collection.findOne({token : data.loser});
-
-
+                    const current = await collection.findOne({ token: data.winner });
+                    const opponent = await collection.findOne({ token: data.loser });
                     if (current && opponent) {
                         console.log(current);
-                        let winnerScore = current.score;
-                        // let newScore = winnerScore !== 0 ? ( winnerScore + ((opponent.score / winnerScore)*10) ) : winnerScore + 10;
-                         let newScore =  winnerScore + 10;
+                        const ELOn = current.score;
+                        const K = 40; // set K to 40 for the first tournament
+                        const W = 1; // assume the winner won
+                        const D = opponent.score - ELOn;
+                        const vD = 1 / (1 + Math.pow(10, -D / 400));
+                        const ELOnew = ELOn + K * (W - vD);
 
+                        let newScore = Math.round(ELOnew); // round to the nearest integer
                         newScore = newScore < 0 ? 0 : newScore;
 
                         let newWins = current.wins + 1;
                         let newTotalGames = current.totalGames + 1;
                         let newValues = { $set: { score: newScore, wins: newWins, totalGames: newTotalGames } };
-                        const result = await collection.updateOne({token : data.winner}, newValues);
+                        const result = await collection.updateOne({ token: data.winner }, newValues);
 
-                        let loserScore = opponent.score;
-                        // let newLoserScore =  loserScore !== 0 ? ( loserScore - ((loserScore / current.score)*10) ) : loserScore - 5;
-                        let newLoserScore =   loserScore - 5;
+                        const loserScore = opponent.score;
+                        const D2 = ELOn - loserScore;
+                        const vD2 = 1 / (1 + Math.pow(10, -D2 / 400));
+                        const ELOnew2 = loserScore + K * (0 - vD2); // assume the loser lost
 
+                        let newLoserScore = Math.round(ELOnew2); // round to the nearest integer
                         newLoserScore = newLoserScore < 0 ? 0 : newLoserScore;
+
                         let newLoses = opponent.loses + 1;
                         let newTotalGamesOp = opponent.totalGames + 1;
-                        let newLoserValues = { $set: { score: newLoserScore, loses : newLoses, totalGames : newTotalGamesOp } };
-                        const result2 = await collection.updateOne({token : data.loser}, newLoserValues);
+                        let newLoserValues = { $set: { score: newLoserScore, loses: newLoses, totalGames: newTotalGamesOp } };
+                        const result2 = await collection.updateOne({ token: data.loser }, newLoserValues);
 
-
-                        if(result && result2){
-                            console.log('Updated score for winner');
+                        if (result && result2) {
+                            console.log('Updated score for winner and loser');
                         }
-                        else{
+                        else {
                             console.log('Something went wrong in the GAMIFICATION update..');
                         }
-
                     }
-                    else{
+                    else {
                         console.log('current or opponent not found..');
                     }
                 } catch (err) {
                     console.error('Failed to connect to db', err);
                 }
             });
-
         }
     }
 }
