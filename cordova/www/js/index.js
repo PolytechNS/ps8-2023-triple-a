80fc5ebe98e520df835d2be9c03fecc0fd0b049b
Anasselayachi <anasselyachi2002@gmail.com>
2023-04-27 11:01:04 +0200

    Plugin notification à afficher sur l'écran

--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -41,3 +41,52 @@ function onDeviceReady() {
     document.getElementById('deviceready').classList.add('ready');
 }
 
+// Attendre que Cordova soit prêt
+document.addEventListener("deviceready", function() {
+  // Créer une notification pour 5 secondes plus tard
+  var date = new Date();
+  date.setSeconds(date.getSeconds() + 20);
+
+  cordova.plugins.notification.local.schedule({
+    title: "Connect4",
+    text: "It's time to play Connect4! Challenge your friends and show them who's the best player!",
+    at: date,
+    foreground: true,
+    wakeup: true
+  });
+}, false);
+
+
+//dans un cas réel , on utilise le code suivant,permettant d'afficher une notification chaque jour à 21:00
+// Récupérer la date et l'heure actuelle
+// var now = new Date();
+
+// // Définir l'heure d'exécution de la notification sur 21h00
+// var notificationTime = new Date();
+// notificationTime.setHours(21);
+// notificationTime.setMinutes(0);
+// notificationTime.setSeconds(0);
+
+// // Vérifier si l'heure actuelle est supérieure ou égale à l'heure d'exécution de la notification
+// if (now >= notificationTime) {
+//   // Si oui, ajouter un jour à la date d'exécution de la notification pour qu'elle soit exécutée demain à 21h00
+//   notificationTime.setDate(notificationTime.getDate() + 1);
+// }
+
+// // Calculer le délai en millisecondes jusqu'à l'heure d'exécution de la notification
+// var delay = notificationTime.getTime() - now.getTime();
+
+// // Définir la fonction de rappel pour exécuter la notification
+// var notificationCallback = function() {
+//   // Code pour afficher la notification ici
+//   var options = {
+//     body: "Challenge your friends and show them who's the best player!",
+//   };
+//   var notification = new Notification("It's time to play Connect4!", options);
+// }
+
+// // Planifier l'exécution de la notification en utilisant setTimeout()
+// setTimeout(notificationCallback, delay);
+
+
+
